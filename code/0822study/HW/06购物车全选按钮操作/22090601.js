4eb9cbc2f8d9885801400c19a47d9ca4eb5a7d87
fan-yue <1819995045@qq.com>
2022-09-06 17:33:26 +0800

    1

--- code/0822study/HW/06购物车全选按钮操作/22090601.js
+++ code/0822study/HW/06购物车全选按钮操作/22090601.js
@@ -17,8 +17,12 @@ $('.addj').click(function(){
     a++;
     $(this).siblings('.atext').val(a);
     // 商品价格计算
-    var b = $('.price').text();
-    $('.zj').text( a * b );
+    var price = $(this).parent().parent().siblings('.pr').find('.price').text();
+
+    $(this).parent().parent().siblings('.priceSum').find('.zj').text( price * a);
+
+    
+    
 })
 
 // 购物车 - 功能
@@ -30,6 +34,17 @@ $('.addy').click(function(){
     $(this).siblings('.atext').val(a);
     
     // 商品价格计算
-    var b = $('.price').text();
-    $('.zj').text( a * b );
+    var price = $(this).parent().parent().siblings('.pr').find('.price').text();
+
+    $(this).parent().parent().siblings('.priceSum').find('.zj').text( price * a);
+})  
+
+$('.atext').change(function(){  
+    // 商品改变的个数
+    var a = $(this).val();
+    // 商品价格
+    var price = $(this).parents().siblings('.pr').find('.price').text();
+
+    // 总价
+    $(this).parents().siblings('.priceSum').find('.zj').text( a * price);
 })
